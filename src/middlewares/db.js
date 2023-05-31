a9ea0813db4d6b712a959e0cafb956f635b755de
Wandering Wizard <107069990+hernandezraymondm@users.noreply.github.com>
2023-05-31 20:15:05 +0800

    added training module

--- src/middlewares/db.js
+++ src/middlewares/db.js
@@ -1,17 +1,18 @@
-const knex = require('knex')({
-  client: 'mysql2',
-  connection: {
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: 'philfida',
-    database: 'tad'
-  },
-  pool: {
-    min: 2,  // minimum number of connections
-    max: 100, // maximum number of connections
-    idleTimeoutMillis: 30000, // connection idle timeout in milliseconds
-    createTimeoutMillis: 3000, // connection creation timeout in milliseconds
-    acquireTimeoutMillis: 3000 // connection acquisition timeout in milliseconds
-  }
-});
+const db = (req, res, next) => {
+  const knex = require('knex')({
+    client: 'mysql2',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'root',
+      password : 'philfida',
+      database : 'tad'
+    }
+  });
+  req.db = knex;
+  next();
+};
+
+module.exports = db;
+
+

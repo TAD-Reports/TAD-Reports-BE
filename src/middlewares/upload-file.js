c593de0dd215c948b5b1c2680918976db0bfd4fb
Wandering Wizard <107069990+hernandezraymondm@users.noreply.github.com>
2023-06-07 01:11:21 +0800

    add Badrequest Error for file size limit

--- src/middlewares/upload-file.js
+++ src/middlewares/upload-file.js
@@ -5,9 +5,9 @@ const upload = multer({
   storage: multer.memoryStorage(),
   fileFilter: function (req, file, cb) {
     if (file.mimetype !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
-      return cb(new BadRequestError('Invalid file type. Only XLSX files are allowed.'))
+      return cb(new BadRequestError('Invalid file type. Only XLSX files are allowed.'));
     }
-    cb(null, true)
+    cb(null, true);
   },
   limits: {
     fileSize: 1024 * 1024 * 100 // 100 MB file size limit
@@ -15,12 +15,19 @@ const upload = multer({
 });
 
 const uploadFile = (req, res, next) => {
-  upload.single('file')(req, res, function(err) {
-    if (err) {
-      return next(err); // Pass the error to the global error handling middleware
+  upload.single('file')(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      // Multer-specific error
+      if (err.code === 'LIMIT_FILE_SIZE') {
+        return next(new BadRequestError('File size exceeds the limit.'));
+      }
+      return next(err);
+    } else if (err) {
+      // Other errors
+      return next(err);
     }
     next();
   });
-}
+};
 
 module.exports = uploadFile;

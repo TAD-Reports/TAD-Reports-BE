f0572d29c5096618970406fa23c30c9e5a4636e0
MarkSalem804 <125126248+MarkSalem804@users.noreply.github.com>
2023-06-12 19:08:40 +0800

    excluded imported_by on cotton and cocoon tables

--- src/app/cocoon/cocoon-store.js
+++ src/app/cocoon/cocoon-store.js
@@ -61,7 +61,7 @@ class CocoonStore {
 
 
   async getExisting(row) {
-    const excludedFields = ["District", "Remarks"];
+    const excludedFields = ["imported_by", "District", "Remarks"];
     const query = this.db(this.table);
     for (const [column, value] of Object.entries(row)) {
       const columnName = column.toLowerCase().replace(/ /g, '_').replace('/', '').replace('(', '').replace(')', '').replace('.','');
--- src/app/cotton/cotton-store.js
+++ src/app/cotton/cotton-store.js
@@ -62,7 +62,7 @@ class CottonStore {
 
 
   async getExisting(row) {
-    const excludedFields = ["District", "Remarks"];
+    const excludedFields = ["imported_by", "District", "Remarks"];
     const query = this.db(this.table);
     for (const [column, value] of Object.entries(row)) {
       const columnName = column.toLowerCase().replace(/ /g, '_').replace('/', '').replace('(', '').replace(')', '');
